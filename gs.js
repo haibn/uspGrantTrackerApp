fdf1250932e4cf08bc8c9e85f4ccc313c25b8fcf
haibn <haibinyuy@berkeley.edu>
2023-07-04 21:13:09 -0700

    app finished

--- gs.js
+++ gs.js
@@ -1,108 +1,109 @@
-// Gets the main html file and runs it
-function doGet(e){
+  // Gets the main html file and runs it
+  function doGet(e){
     return HtmlService.createTemplateFromFile('index').evaluate();
-}
+  }
   
-// Helper function used to link multiple html files (Used to link stylesheet.html and js.html to index.html)
-function include(filename){ 
+  // Helper function used to link multiple html files (Used to link stylesheet.html and js.html to index.html)
+  function include(filename){ 
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
-}
+  }
   
-// Helper function that counts how many times a value is repeated in an array
-function getOccurrence(array, value) {
-    var count = 0;
-    array.forEach((v) => (v === value && count++));
-    return count;
-}
-
-// Helper function that gets all indexes of a ocurrence in an array
-function getAllIndexes(arr, val) {
-    var indexes = [], i;
-    for(i = 0; i < arr.length; i++)
-        if (arr[i] === val)
-            indexes.push(i);
-    return indexes;
-}
-
-function newAccount(params) {
-    let name = params[0];
+  // Helper function that counts how many times a value is repeated in an array
+  function getOccurrence(array, value) {
+      var count = 0;
+      array.forEach((v) => (v === value && count++));
+      return count;
+  }
+  
+  // Helper function that gets all indexes of a ocurrence in an array
+  function getAllIndexes(arr, val) {
+      var indexes = [], i;
+      for(i = 0; i < arr.length; i++)
+          if (arr[i] === val)
+              indexes.push(i);
+      return indexes;
+  }
+  
+  // Access the database, authenticates the provided sign up data, and stores the new data for the new account.
+  function newAccount(params) {
+    // retrieves the sid inputted by the user converting it to a Number type for comparison purposes
     let sid = Number(params[1]);
-    let password = params[2];
+  
+    // access the database
     let url = "https://docs.google.com/spreadsheets/d/11scOkr3HQhseyUir8YgwOabW3PwbeDA7wTVJUyuK2L8/edit#gid=916264064";
     let ss = SpreadsheetApp.openByUrl(url);
+    // access to the "Emergency Grant Tracker sheet and the "Form Responses 1" sheet
     let wsAccounts = ss.getSheetByName("Emergency Grant Tracker");
     let wsResponses = ss.getSheetByName("Form Responses 1");
-    // Logger.log(params);
+    // get the list of sids in the "Emergency Grant Tracker" sheet and "Form Responses 1" sheet
     let wsAccountsSids = wsAccounts.getRange(2,2,wsAccounts.getLastRow(), 1).getValues();
     let wsResponsesSids = wsResponses.getRange(2,5,wsResponses.getLastRow(), 1).getValues();
     let accountsSidList = wsAccountsSids.map(function(r) { return r[0]; });
     let reponsesSidList = wsResponsesSids.map(function(r) { return r[0]; });
+    
+    // checks if the provided sid exists in the database and if there's an account already with the given sid
     let accPosition = accountsSidList.indexOf(sid);
     let resPosition = reponsesSidList.indexOf(sid);
-    // Logger.log("sid: " + sid + " typeof " + typeof(sid));
-    // Logger.log("reponsesSidList: " + reponsesSidList + " typeof " + typeof(reponsesSidList[0]));
-    // Logger.log("resPosition: " + resPosition);
-    // Logger.log("accountsSidList: " + accountsSidList + " typeof " + typeof(accountsSidList[0]));
-    // Logger.log("accPosition: " + accPosition);
     if (resPosition > -1) {
-        if (accPosition > -1) {
-            throw Error("Account with the given SID already exists.");
-        } else {
-            wsAccounts.appendRow(params);
-        }
+      if (accPosition > -1) {
+        throw Error("Account with the given SID already exists.");
+      } else {
+        wsAccounts.appendRow(params);
+      }
     } else {
-        throw Error("SID not found in our records.");
+      throw Error("SID not found in our records.");
     }
-}
-
-function newUserLogIn(params) {
+  }
+  
+  // Access the database, retrieves user's information and send the information back to the frontend.
+  function newUserLogIn(params) {
+    // retrieves the sid and password inputted by the user (converting the sid to a Number type for comparison purposes)
     let sid = Number(params[0]);
     let password = params[1];
+  
+    // access the database
     let url = "https://docs.google.com/spreadsheets/d/11scOkr3HQhseyUir8YgwOabW3PwbeDA7wTVJUyuK2L8/edit#gid=916264064";
     let ss = SpreadsheetApp.openByUrl(url);
-
+    // get the list of sids in the "Form Responses 1" sheet
     let wsResponses = ss.getSheetByName("Form Responses 1");
     let wsResponsesSids = wsResponses.getRange(2,5,wsResponses.getLastRow(), 1).getValues();
     let reponsesSidList = wsResponsesSids.map(function(r) { return r[0]; });
-
+    // get the list of sids and passwords in the "Emergency Grant Tracker" sheet
     let wsAccounts = ss.getSheetByName("Emergency Grant Tracker");
     let wsAccountsSids = wsAccounts.getRange(2,2,wsAccounts.getLastRow(), 1).getValues();
     let wsAccountsPasswords = wsAccounts.getRange(2,3,wsAccounts.getLastRow(), 1).getValues();
     let sidList = wsAccountsSids.map(function(r) { return r[0]; });
     let passwordList = wsAccountsPasswords.map(function(r) { return r[0]; });
+  
+    // check if there's an existing account with the given sid
     let position = sidList.indexOf(sid);
-
     if (position > -1) {
-        if (password == passwordList[position]) {
+      // if so check if the given password matches the on in the database
+      if (password == passwordList[position]) {
+        // retrieves the user's data from the database and sends it back to the frontend
         let resPositions = getAllIndexes(reponsesSidList, sid);
-        resPositions = resPositions.map(v=> v+2);
+        resPositions = resPositions.map(v=> v+2)
         let data = {
-            nameCell : [],
-            sidCell : [],
-            dateCell : [],
-            awardCell : [],
-            statusCell : [],
-            notesCell : []
+          nameCell : [],
+          sidCell : [],
+          dateCell : [],
+          awardCell : [],
+          statusCell : [],
+          notesCell : []
         }
         for (i = 0; i < resPositions.length; i ++) {
-            data.nameCell.push(wsResponses.getRange('C' + resPositions[i]).getValue());
-            data.sidCell.push(wsResponses.getRange('E' + resPositions[i]).getValue());
-            data.dateCell.push(wsResponses.getRange('A' + resPositions[i]).getValue().toISOString().substring(0, 10));
-            data.awardCell.push(wsResponses.getRange('M' + resPositions[i]).getValue());
-            data.statusCell.push(wsResponses.getRange('Z' + resPositions[i]).getValue());
-            data.notesCell.push(wsResponses.getRange('AA' + resPositions[i]).getValue());
+          data.nameCell.push(wsResponses.getRange('C' + resPositions[i]).getValue());
+          data.sidCell.push(wsResponses.getRange('E' + resPositions[i]).getValue());
+          data.dateCell.push(wsResponses.getRange('A' + resPositions[i]).getValue().toISOString().substring(0, 10));
+          data.awardCell.push(wsResponses.getRange('M' + resPositions[i]).getValue());
+          data.statusCell.push(wsResponses.getRange('Z' + resPositions[i]).getValue());
+          data.notesCell.push(wsResponses.getRange('AA' + resPositions[i]).getValue());
         }
-            Logger.log("nameCell " + data.nameCell);
-            Logger.log("sidCell " + data.sidCell);
-            Logger.log("dateCell " + data.dateCell + " typeof " + typeof(data.dateCell[0]));
-            Logger.log("awardCell " + data.awardCell);
-            Logger.log("statusCell " + data.statusCell);
-            Logger.log("notesCell " + data.notesCell);
-            return data;
-        } else {
-            throw Error("Incorrect password.");
-        }
+        return data;
+      } else {
+        throw Error("Incorrect password.");
+      }
     } else {
-        throw Error("User not found.");
+      throw Error("User not found.");
     }
-}
+  }
--- script.js
+++ script.js
@@ -1,96 +1,154 @@
 //signup Student Information button. When clicked, it displays the form. 
 function signupForm() {
-    var home = document.getElementById("home");
-    var form = document.getElementById("signup");
-    console.log(form);
-    home.style.display = "none";
-    form.style.display = "block";
+  var home = document.getElementById("home");
+  var form = document.getElementById("signup");
+  console.log(form);
+  home.style.display = "none";
+  form.style.display = "block";
 }
 
 //login Student Information button. When clicked, it displays the form. 
 function loginForm() {
-    var home = document.getElementById("home");
-    var form = document.getElementById("login");
-    console.log(form);
-    home.style.display = "none";
-    form.style.display = "block";
+  var home = document.getElementById("home");
+  var form = document.getElementById("login");
+  console.log(form);
+  home.style.display = "none";
+  form.style.display = "block";
 }
 
 // Back button. When clicked, it returns to the home page
 function back() {
-    var home = document.getElementById("home");
-    var signupForm = document.getElementById("signup");
-    var loginForm = document.getElementById("login");
-    let summary = document.getElementById("summary");
+  var home = document.getElementById("home");
+  var signupForm = document.getElementById("signup");
+  var loginForm = document.getElementById("login");
+  let summary = document.getElementById("summary");
+  let records = document.getElementById("records");
 
-    if (signupForm.style.display == "block") {
-      home.style.display = "block";
-      signupForm.style.display = "none";
-    } else if (summary.style.display == "block") {
-      home.style.display = "block";
-      summary.style.display = "none";
-      loginForm.style.display = "none";
-    } else {
-      home.style.display = "block";
-      loginForm.style.display = "none";
-    }
+  if (signupForm.style.display == "block") {
+    home.style.display = "block";
+    signupForm.style.display = "none";
+  } else if (summary.style.display == "block") {
+    home.style.display = "block";
+    summary.style.display = "none";
+    loginForm.style.display = "none";
+  } else {
+    home.style.display = "block";
+    loginForm.style.display = "none";
+  }
+
+  records.textContent = "";
 }
 
-// Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
+// Failure Handler function used to alert the user if the inputted SID is not found in the database (google sheets)
 function accountNotCreated(error) {
-    alert(error);
+  alert(error);
 }
 
-// Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
+// Success Handler function to let the user know the account was created successfully
 function accountCreatedSuccessfully() {
-    alert("Account Created Successfully!");
+  alert("Account Created Successfully!");
 }
 
-// Function used to add the given infomation inputted by the user on the web app to google sheets.
-// This function is triggered when the "add" button is clicked.
+// Checks that every field is filled out and creates a new account calling newAccount() from Code.gs
 function createAccount() {
-    let name = document.getElementById("nameSignup");
-    let sid = document.getElementById("sidSignup");
-    let password = document.getElementById("passwordSignup");
-    let params = [name.value, sid.value, password.value];
-    if (name.value != "" && sid.value != "" && password.value != "") {
-      google.script.run.withSuccessHandler(accountCreatedSuccessfully).withFailureHandler(accountNotCreated).newAccount(params);
-      name.value = "";
-      sid.value = "";
-      password.value = "";
-    } else {
-      alert("Missing fields.");
-    }
-}
-
-// Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
-function unsuccessfulLogIn(error) {
-    alert(error);
-}
-
-// Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
-function loggedIn(data) {
-    console.log(data);
-    let name = data.nameCell;
-    let sid = data.sidCell;
-    let date = data.dateCell;
-    let award = data.awardCell;
-    let status = data.statusCell;
-    let note = data.notesCell;
-    console.log(name)
-    console.log(sid)
-    console.log(date)
-    console.log(award)
-    console.log(status)
-    console.log(note)
-    alert("Logged in!");
-}
-
-function userLogIn() {
-    let sid = document.getElementById("sidlogin");
-    let password = document.getElementById("passwordlogin");
-    let params = [sid.value, password.value];
-    google.script.run.withSuccessHandler(loggedIn).withFailureHandler(unsuccessfulLogIn).newUserLogIn(params);
+  let name = document.getElementById("nameSignup");
+  let sid = document.getElementById("sidSignup");
+  let password = document.getElementById("passwordSignup");
+  let params = [name.value, sid.value, password.value];
+  if (name.value != "" && sid.value != "" && password.value != "") {
+    google.script.run.withSuccessHandler(accountCreatedSuccessfully).withFailureHandler(accountNotCreated).newAccount(params);
+    name.value = "";
     sid.value = "";
     password.value = "";
+  } else {
+    alert("Missing fields.");
+  }
+}
+
+// Failure Handler function used to alert the user that he could not login
+function unsuccessfulLogIn(error) {
+  alert(error);
+}
+
+// Success Handler function that retrieves the data from the backend (google sheets database) and displays the information to the user.
+function loggedIn(data) {
+  // stores retrieved data in vars
+  let name = data.nameCell;
+  let sid = data.sidCell;
+  let date = data.dateCell;
+  let award = data.awardCell;
+  let status = data.statusCell;
+  let note = data.notesCell;
+
+  // access the summary, form and records section on index.html
+  let summary = document.getElementById("summary");
+  let form = document.getElementById("login");
+  let records = document.getElementById("records");
+
+  // stores user's name and sid on index.html
+  let newName = document.getElementById("summName");
+  let newSid = document.getElementById("summSid");
+  newName.innerHTML = name[0];
+  newSid.innerHTML = sid[0];
+
+  // loops through the retrieved data and appends it to the records section on index.html
+  for (i = 0; i < sid.length; i ++) {
+    let newRecord = document.createElement("li");
+
+    let bDate = document.createElement("b");
+    let bAward = document.createElement("b");
+    let bStatus = document.createElement("b");
+    let bNote = document.createElement("b");
+
+    bAward.textContent = "Award: ";
+    bStatus.textContent = "Status: ";
+    bNote.textContent = "Comments: ";
+
+    let spanDate = document.createElement("span");
+    let spanAward = document.createElement("span");
+    let spanStatus = document.createElement("span");
+    let spanNote = document.createElement("span");
+
+    spanDate.textContent = date[i];
+    spanDate.setAttribute("class", "dateStyle");
+
+    spanAward.textContent = award[i];
+
+    if (status[i] == "") {
+      spanStatus.textContent = "Status yet to be assigned";
+    } else {
+      spanStatus.textContent = status[i];
+    }
+    if (note[i] == "") {
+      spanNote.textContent = "No comments";
+    } else {
+      spanNote.textContent = note[i];
+    }
+
+    bDate.appendChild(spanDate);
+    bAward.appendChild(spanAward);
+    bStatus.appendChild(spanStatus);
+    bNote.appendChild(spanNote);
+
+    newRecord.appendChild(bDate);
+    newRecord.appendChild(bAward);
+    newRecord.appendChild(bStatus);
+    newRecord.appendChild(bNote);
+
+    records.appendChild(newRecord);
+  }
+
+  // displays student summary and hides log in form
+  form.style.display = "none";
+  summary.style.display = "block";
+}
+
+// Gets the user log in information and sends it to the backend by calling newUserLogIn()
+function userLogIn() {
+  let sid = document.getElementById("sidlogin");
+  let password = document.getElementById("passwordlogin");
+  let params = [sid.value, password.value];
+  google.script.run.withSuccessHandler(loggedIn).withFailureHandler(unsuccessfulLogIn).newUserLogIn(params);
+  sid.value = "";
+  password.value = "";
 }
